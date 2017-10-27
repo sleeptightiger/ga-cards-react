adaac123f3044ed54f81dacb5be909b091d00731
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-10-27 12:11:41 -0500

    card flipping functionality functional

--- cards-react/src/components/Card.js
+++ cards-react/src/components/Card.js
@@ -3,13 +3,29 @@ import React, { Component } from 'react';
 import '../css/Card.css';
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFlipped: false
+    }
+    this.flipCard = this.flipCard.bind(this);
+  }
+
+  flipCard(e) {
+    this.setState({
+      isFlipped: !this.state.isFlipped
+    })
+  }
+
   render() {
-    console.log(this.props.title);
+
+    let title = this.state.isFlipped ? this.props.title + ' ________' : '';
+
     return (
-      <div className="Card">
+      <div className="Card" onClick={this.flipCard}>
         <div className="col-sm-6 col-md-4 col-lg-4">
           <div className="card">
-            <h4 className="card-title">{this.props.title}</h4>
+            <h4 className="card-title">{title}</h4>
             <h6>Cards Against Assembly</h6>
           </div>
         </div>
