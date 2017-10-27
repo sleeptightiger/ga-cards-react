db82c921f69d5c1d0bc607e676ca87f9ef9a624f
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-10-27 11:30:15 -0500

    nav router working... mostly

--- cards-react/src/App.js
+++ cards-react/src/App.js
@@ -4,6 +4,7 @@ import CardList from './components/CardList';
 import AddCard from './components/AddCard';
 import Footer from './components/Footer';
 import About from './components/About';
+import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
 import './css/App.css';
 
 class App extends Component {
@@ -12,13 +13,20 @@ class App extends Component {
       <div className="container">
         <Header />
 
-        <nav className="tabs">
-          <a href="index.html" className="active">Home</a>
-          <a href="add.html">Add a New Card</a>
-          <a href="about.html">About</a>
-        </nav>
-
-        <About />
+        <Router>
+          <div>
+            <nav className="tabs">
+              <NavLink exact className="link" to="/">Home</NavLink>{' '}
+              <NavLink className="link" to="/newCard">Add a New Card</NavLink>{' '}
+              <NavLink className="link" to="/about">About</NavLink>{' '}
+            </nav>
+            <Switch>
+              <Route exact path="/" component={CardList} />
+              <Route path="/newCard" component={AddCard}/>
+              <Route path="/about" component={About}/>
+            </Switch>
+          </div>
+        </Router>
 
         <Footer />
       </div>
